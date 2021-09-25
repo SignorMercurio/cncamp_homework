f826dfdd207f7b9d383fb77a733ed4bb21188929
Mercurio <10175101282@stu.ecnu.edu.cn>
2021-09-25 16:12:39 +0100

    Add features requested in homework

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SignorMercurio/cncamp_homework
 
 go 1.16
+
+require github.com/valyala/fasthttp v1.30.0
--- httpserver/http.go
+++ httpserver/http.go
@@ -1,31 +1,53 @@
 package httpserver
 
 import (
-	"fmt"
 	"log"
-	"net/http"
+	"os"
+
+	"github.com/valyala/fasthttp"
 )
 
 func NewServer(addr string) error {
-	http.HandleFunc("/", http.HandlerFunc(defaultHandler))
-
-	log.Printf("Listening on %s...", addr)
-
-	return http.ListenAndServe(addr, nil)
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Here are your HTTP Request Headers:\n\n")
-	printHeaders(w, r.Header)
-
-	fmt.Fprint(w, "Here are your HTTP Request Headers:\n\n")
-	printHeaders(w, r.Response.Header)
-}
-
-func printHeaders(w http.ResponseWriter, h http.Header) {
-	for name, headers := range h {
-		for _, header := range headers {
-			fmt.Fprintf(w, "%s: %s\n", name, header)
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		switch string(ctx.Path()) {
+		case "/reqHdr":
+			injectResquestHeaders(ctx)
+		case "/getEnv":
+			getEnv(ctx)
+		case "/log":
+			writeLog(ctx)
+		case "/healthz":
+			healthCheck(ctx)
+		default:
+			ctx.Error("Path not found", fasthttp.StatusNotFound)
 		}
 	}
+	log.Printf("Listening on %s...", addr)
+
+	return fasthttp.ListenAndServe(addr, requestHandler)
+}
+
+func injectResquestHeaders(ctx *fasthttp.RequestCtx) {
+	ctx.Request.Header.VisitAll(func(key, value []byte) {
+		ctx.Response.Header.Set("X-"+string(key), string(value))
+	})
+	ctx.Response.SetBody([]byte(`Check the Request Headers in Responses Headers (those with the "X" prefix)`))
+}
+
+func getEnv(ctx *fasthttp.RequestCtx) {
+	v := os.Getenv("VERSION")
+	if v == "" {
+		v = "0.0.0"
+	}
+	ctx.Response.Header.Set("X-Version", v)
+	ctx.Response.SetBody([]byte(`Check the version in Response Headers`))
+}
+
+func writeLog(ctx *fasthttp.RequestCtx) {
+	log.Printf("IP: %s; Status: %d\n", ctx.RemoteAddr().String(), ctx.Response.StatusCode())
+	ctx.Response.SetBody([]byte(`This access attempt has been logged.`))
+}
+
+func healthCheck(ctx *fasthttp.RequestCtx) {
+	ctx.Response.SetBody([]byte("Awesome"))
 }
